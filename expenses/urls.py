d016a90678cae724eba631875bd2fdd3d536df89
R O K O N 💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-03-31 10:05:44 +0600

    Update urls.py

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add/', views.add_transaction, name='add_transaction'),
+    path('transactions/', views.transaction_list, name='transaction_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
