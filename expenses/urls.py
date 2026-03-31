f2ba5eecc13acafa9d910a5e8f0dc05db0f9ece1
R O K O N 💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-03-31 10:25:17 +0600

    Update urls.py

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('export-csv/', views.export_csv, name='export_csv'),
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add/', views.add_transaction, name='add_transaction'),
